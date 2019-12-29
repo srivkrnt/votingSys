d55f6442520854229e5aa242c0ba7b69fca11191
srivkrnt <sri.vkrnt@gmail.com>
2019-12-29 18:26:41 +0530

    revert changes

--- manage.py
+++ manage.py
@@ -3,9 +3,59 @@ from flask_sqlalchemy import SQLAlchemy
 from config import Config
 from services import aadharService,otherService
 import json
-from __init__ import set
 
-app,db = set()
+app = Flask(__name__)
+app.config.from_object(Config)
+app.secret_key = 'SECRET'
+db = SQLAlchemy(app)
+
+class Voter(db.Model):
+    """ Voter model for storing voters """
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100))
+    username = db.Column(db.String(100))
+    password = db.Column(db.String(100))
+    age = db.Column(db.Integer)
+    phone = db.Column(db.String(10))
+    aadharNumber = db.Column(db.String(12))
+    address = db.Column(db.String(200))
+    voted = db.Column(db.Boolean)
+
+    def __init__(self, name, username, password, age, phone, aadharNumber, address):
+        self.name = name
+        self.username = username
+        self.password = password
+        self.age = age
+        self.phone = phone
+        self.aadharNumber = aadharNumber
+        self.address = address
+        self.voted = False
+
+class Message(db.Model):
+    """ For Internal use of Phone Service """
+    id = db.Column(db.Integer, primary_key=True,autoincrement=True)
+    phone = db.Column(db.String(10))
+    otp = db.Column(db.String(10))
+
+    def __init__(self, phone, otp):
+        self.phone = phone
+        self.otp = otp
+
+class Votes(db.Model):
+    """ For maintaining votes """
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    aadharNumber = db.Column(db.String(12))
+
+    def __init__(self, aadharNumber):
+        self.aadharNumber = aadharNumber
+
+class Log(db.Model):
+    """ For maintaining votes """
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    candidate = db.Column(db.String(12))
+
+    def __init__(self, candidate):
+        self.candidate = candidate
 
 @app.route('/verify/aadhar/<aadhar_number>')
 def verifyAadharNumber(aadhar_number):
